8a80270e0f1d8543bfcf75faf78b98cc941f7359
s-k3nyyy <collo.c.heg@gmail.com>
2024-05-09 21:45:17 +0300

    updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,10 @@
 import './Header.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
-<<<<<<< HEAD
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-=======
 
  
->>>>>>> b359e7299511ee45f4476d8517459dea754a6516
 
 function Header() {
   return (
@@ -16,7 +13,6 @@ function Header() {
         <ul>
           <h1 className='logo'> <span className='redd'>RED</span><span className='drip'>DRIP</span> <span className='bblood'>&#x1FA78;</span> </h1>      
           <li><Link to="/">Home</Link></li>
-<<<<<<< HEAD
           <li><Link to="/patient">Patient</Link></li>
           <li><Link to="/bloodbanks">bloodbank</Link></li>
       <FontAwesomeIcon icon="fa-solid fa-bed" />
@@ -24,17 +20,6 @@ function Header() {
                <FontAwesomeIcon icon="fa-solid fa-bed" />
           <li><Link to="/admin">Admin</Link></li>
           <FontAwesomeIcon icon="fa-solid fa-lock" />
-=======
-          
-          <li><Link to="/patient">Patient</Link></li>
-          
-          <li><Link to="/bloodbanks">bloodbank</Link></li>
-      
-          <li><Link to="/donor">Donor</Link></li>
-               
-          <li><Link to="/admin">Admin</Link></li>
-          
->>>>>>> b359e7299511ee45f4476d8517459dea754a6516
          
         </ul>
       </nav>
